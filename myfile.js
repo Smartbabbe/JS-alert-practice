fbd56256097df8d79bc5f42667ab76126495aedc
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2022-06-24 08:54:35 +0100

    Update myfile.js

--- myfile.js
+++ myfile.js
@@ -1,7 +1,7 @@
 var Name = "Esther Israel Okon";
-var Age = "19";
+var Height = "5'9";
 var Country = "Nigeria"
 
-alert('My name is ' +Name + ',' + ' I am ' +Age + ' years old,' + ' and My country is ' + Country)
+alert('My name is ' +Name + ',' + ' I am ' +Height + ' tall,' + ' and My country is ' + Country)
 
-console.log('My name is ' +Name + ',' + ' I am ' +Age + ' years old,' + ' and My country is ' + Country)
+console.log('My name is ' +Name + ',' + ' I am ' +Height + ' tall,' + ' and My country is ' + Country)
