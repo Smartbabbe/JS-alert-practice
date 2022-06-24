d414864efbc7bbe2bc08a74d55e961f17ee6f664
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2022-06-24 08:52:31 +0100

    my js files

---
+++ myfile.js
@@ -0,0 +1,7 @@
+var Name = "Esther Israel Okon";
+var Age = "19";
+var Country = "Nigeria"
+
+alert('My name is ' +Name + ',' + ' I am ' +Age + ' years old,' + ' and My country is ' + Country)
+
+console.log('My name is ' +Name + ',' + ' I am ' +Age + ' years old,' + ' and My country is ' + Country)
